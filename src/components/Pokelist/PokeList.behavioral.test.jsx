b494e35e9d8e5e07e68510d4aa5e4ffffc4b3a13
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-03 02:49:12 -0700

    created state for POkeList

---
+++ src/components/Pokelist/PokeList.behavioral.test.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+
+export default function PokeList() {
+    const [pokemon, setPokemon] = useState([]);
+    const [searchedPokemon, setSearchedPokemon] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState('');
+
+
+    
+}
