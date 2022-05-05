ef2d4aea63c42a35c895510c73e3c193a7598ec4
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-04 17:55:27 -0700

    rewrote components test with a few other components to be tested

--- src/components/Pokelist/PokeList.test.jsx
+++ src/components/Pokelist/PokeList.test.jsx
@@ -1,14 +1,19 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import PokeList from './PokeList';
 
 describe('PokeList', () => {
     it('should render the Pokemon', async () => {
         render(<PokeList />)
+        await waitForElementToBeRemoved(screen.getByText('Loading'));
+
+        await screen.findAllByRole('img', { alt: 'pic of pokemon' });
+
+        await screen.findByLabelText('Name');
 
         return waitFor(() => {
             const pikachuName = screen.getByText('pikachu');
 
             expect(pikachuName).toHaveTextContent('pikachu');
-        });
+        }, { timeout: 5000 });
     });
 });
